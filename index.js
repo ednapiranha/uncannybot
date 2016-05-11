01aeb0db1a5b575115592a9458dd22489917ecb8
Jen <jfong@mozilla.com>
2016-05-10 20:47:30 -0700

    basic listen/response

--- index.js
+++ index.js
@@ -1,31 +1,84 @@
 'use strict';
 
-var irc = require('irc');
-var nconf = require('nconf');
+const RtmClient = require('@slack/client').RtmClient;
+const RTM_CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+const nconf = require('nconf');
 
 nconf.argv().env().file({ file: 'config.json' });
 
-var client = new irc.Client(nconf.get('server'), nconf.get('botName'), {
-  channels: nconf.get('channels'),
+const rtm = new RtmClient(nconf.get('slackKey'));
+
+let channels = {};
+let uid;
+
+rtm.start();
+
+function sendResponse(data) {
+  if (data.text.match(/(lo{1,}l|haha|hehe)/gi)) {
+    rtm.sendMessage('lol!', data.channel);
+    return;
+  }
+
+  if (data.text.match(/wonky/gi) || data.text.indexOf('@' + uid) > -1) {
+    if (data.text.match(/thanks/gi)) {
+      rtm.sendMessage("You're welcome :D", data.channel);
+      return;
+    }
+
+    if (data.text.match(/you’re weird/gi)) {
+      rtm.sendMessage('I am the walrus', data.channel);
+      return;
+    }
+
+    rtm.sendMessage('Are you talking to me? I am waiting for Godot.', data.channel);
+    return;
+  }
+}
+
+function checkMessageType(data) {
+  if (data.subtype) {
+    switch (data.subtype) {
+      case 'channel_join':
+        channels[data.channel] = data.channel;
+        rtm.sendMessage("Hi, I'm just saying hi that's all. Ok.", data.channel);
+        break;
+
+      default:
+        break;
+    }
+
+    return;
+  }
+
+  if (data.text) {
+    sendResponse(data);
+  }
+}
+
+rtm.on(RTM_CLIENT_EVENTS.RTM.AUTHENTICATED, (data) => {
+  uid = data.self.id;
+  console.log('connected');
 });
 
-nconf.get('channels').forEach(function (channel) {
-  client.join(channel, function () {
-    console.log('connected to ', channel);
-    client.say('NickServ', 'IDENTIFY ' + nconf.get('password'));
-  });
-});
+rtm.on(RTM_EVENTS.MESSAGE, (data) => {
+  switch (data.type) {
+    case 'message':
+      checkMessageType(data);
+      break;
 
-client.addListener('registered', function (message) {
-  console.log('registered ', message);
-});
-
-client.addListener('message', function (from, to, message) {
-  if (message.match(/jbuck/i)) {
-    client.say(to, 'WE WILL BUILD A WALL AND MAKE JBUCK PAY FOR IT.');
+    default:
+      break;
   }
 });
 
-client.addListener('error', function (message) {
-  console.error('error: ', message);
+rtm.on(RTM_EVENTS.REACTION_ADDED, (data) => {
+  switch (data.type) {
+    case 'reaction_added':
+      console.log('noted reaction ', data.item.ts);
+      break;
+
+    default:
+      break;
+  }
 });
